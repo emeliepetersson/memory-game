2ae23d976eaabeba3f3b8f71a76f78187f2a6a08
emeliepetersson <emepet1204@skola.goteborg.se>
2019-11-26 10:07:56 +0100

    add files

---
+++ main.js
@@ -0,0 +1,8 @@
+// Array with memory-card, 10st
+
+// Create each memory-card two times each
+
+// Play-button som vid klick visar alla korten och shufflar dem
+
+// Restart-button som visas när alla kort har hittats, 
+//som vänder på korten och shufflar ordningen
