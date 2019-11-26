7c2c7c19c74cb8d397041ee93a271c652970c15c
emeliepetersson <emepet1204@skola.goteborg.se>
2019-11-26 11:44:29 +0100

    create cards

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
+const memoryBoard = document.querySelector('.memory-board');
+
 // Array with memory-card, 10st
+const cards = [
+    { name: "blown", image: "/images/blown.png", icon: "/images/magnifyer.png"},
+    { name: "devil", image: "/images/devil.png", icon: "/images/magnifyer.png"},
+    { name: "kiss", image: "/images/kiss.png", icon: "/images/magnifyer.png"},
+    { name: "party", image: "/images/party.png", icon: "/images/magnifyer.png"},
+    { name: "poop", image: "/images/poop.png", icon: "/images/magnifyer.png"},
+    { name: "sad", image: "/images/sad.png", icon: "/images/magnifyer.png"},
+    { name: "ssh", image: "/images/ssh.png", icon: "/images/magnifyer.png"},
+    { name: "stars", image: "/images/stars.png", icon: "/images/magnifyer.png"}
+]
+
 
 // Create each memory-card two times each
+
+const createCard = (name, frontIcon, backImage) => {
+    const card = document.createElement('div');
+    const front = document.createElement('img');
+    const back = document.createElement('img');
+    
+    //front side
+    front.src = frontIcon;
+    front.classList.add('front');
+    
+    //back side
+    back.src = backImage;
+    back.classList.add('back');
+    
+    //the card
+    card.classList.add('memory-card');
+    card.dataset.emoji = name;
+    card.appendChild(front);
+    card.appendChild(back);
+    return card;
+  };
+
+cards.forEach(card => {
+    const element = createCard(card.name, card.icon, card.image);
+    const clone = element.cloneNode(true);
+    memoryBoard.appendChild((element));
+    memoryBoard.appendChild((clone));
+});
+
 
 // Play-button som vid klick visar alla korten och shufflar dem
 
