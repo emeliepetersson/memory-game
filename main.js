4073878e31b8b589719481796abfef76de215599
emeliepetersson <emepet1204@skola.goteborg.se>
2019-11-26 10:30:01 +0100

    add comments in js

--- main.js
+++ main.js
@@ -4,5 +4,9 @@
 
 // Play-button som vid klick visar alla korten och shufflar dem
 
+// När ett kort klickas vänds det. Vid tredje klicket: 
+// Om två element med samma dataset värde klickas i stannar dem 
+//uppvända, annars vänds dem igen.
+
 // Restart-button som visas när alla kort har hittats, 
 //som vänder på korten och shufflar ordningen
