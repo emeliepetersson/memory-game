e4c8105339caa2ff9ba2734644bb28c13f2e0dc9
emeliepetersson <emepet1204@skola.goteborg.se>
2019-11-27 20:34:54 +0100

    add functions in js

--- main.js
+++ main.js
@@ -44,11 +44,80 @@ cards.forEach(card => {
 });
 
 
-// Play-button som vid klick visar alla korten och shufflar dem
 
 // När ett kort klickas vänds det. Vid tredje klicket: 
 // Om två element med samma dataset värde klickas i stannar dem 
 //uppvända, annars vänds dem igen.
+
+const memoryCards = document.querySelectorAll('.memory-card');
+let cardIsFlipped = false;
+let lockBoard = false;
+let firstCard;
+let secondCard;
+
+
+function flipCard (event) {
+    if(lockBoard) return;
+    if(event.currentTarget === firstCard) return;
+
+    event.currentTarget.classList.toggle('flip');
+    
+    if(!cardIsFlipped) {
+        //First click
+        cardIsFlipped = true;
+        firstCard = event.currentTarget;
+    } else {
+        //Second click
+        cardIsFlipped = false;
+        secondCard = event.currentTarget;
+        
+        checkForMatch();
+    }
+
+}
+
+function checkForMatch() {
+    
+    if(firstCard.dataset.emoji === secondCard.dataset.emoji) {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+
+        resetBoard();
+    } else {
+        lockBoard = true;
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+
+            resetBoard();
+        }, 1500);
+    }
+}
+
+function resetBoard() {
+    [cardIsFlipped, lockBoard] = [false, false]
+    [firstCard, secondCard] = [null, null]
+}
+
+memoryCards.forEach(card => card.addEventListener('click', flipCard));
+
+// Play-button som vid klick visar alla korten och shufflar dem
+const startButton = document.querySelector('.start-button');
+
+function startGame (event) {
+    event.currentTarget.classList.add('hide');
+    memoryBoard.classList.add('show');
+    shuffleCards();
+}
+
+function shuffleCards() {
+    memoryCards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+}
+
+startButton.addEventListener('click', startGame);
 
 // Restart-button som visas när alla kort har hittats, 
 //som vänder på korten och shufflar ordningen
